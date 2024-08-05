b7c831e31cf31564dff48ae4dd08e5373d800cdf
경아 <ukyunga0.1@gmail.com>
2024-08-05 16:57:46 +0900

    FEAT: project 섹션 90% 구현완료

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import Lenis from 'lenis';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
 const colorModes = [
-    { background: '#FFECDD', color: '#FA3701', borderColor: '#FA3701' },
-    { background: '#3B5B8B', color: '#EEC887', borderColor: '#EEC887' },
-    { background: '#F299C9', color: '#F14AA4', borderColor: '#F14AA2' },
-    { background: '#645C31', color: '#ED96C0', borderColor: '#ED96C0' },
-    { background: '#1F6246', color: '#E8B3C3', borderColor: '#E8B3C3' },
-    { background: '#F3EEE3', color: '#4340A0', borderColor: '#4340A0' },
+    { background: '#FFECDD', color: '#FA3701', borderColor: '#FA3701', hoverColor: '#de3304' },
+    { background: '#3B5B8B', color: '#EEC887', borderColor: '#EEC887', hoverColor: '#ddb97d' },
+    { background: '#F299C9', color: '#F14AA4', borderColor: '#F14AA2', hoverColor: '#de4294' },
+    { background: '#645C31', color: '#ED96C0', borderColor: '#ED96C0', hoverColor: '#b87595' },
+    { background: '#1F6246', color: '#E8B3C3', borderColor: '#E8B3C3', hoverColor: '#cd9dab' },
+    { background: '#F3EEE3', color: '#4340A0', borderColor: '#4340A0', hoverColor: '#383585' },
 ];
 
 function App() {
@@ -29,6 +29,7 @@ function App() {
         root.style.setProperty('--background-color', newMode.background);
         root.style.setProperty('--text-color', newMode.color);
         root.style.setProperty('--border-color', newMode.borderColor);
+        root.style.setProperty('--hover-color', newMode.hoverColor);
     };
 
     useEffect(() => {
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,78 +1,132 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-
-// ScrollTrigger 플러그인을 GSAP에 등록합니다.
-gsap.registerPlugin(ScrollTrigger);
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Project = () => {
-    // h2와 ul을 참조하기 위한 ref
-    const h2Ref = useRef();
-    const ulRef = useRef();
+    // 각 박스의 호버 상태를 배열로 관리
+    const [hoverIndex, setHoverIndex] = useState(null);
 
-    // useEffect(() => {
-    //     // H2 애니메이션
-    //     gsap.fromTo(
-    //         h2Ref.current,
-    //         { scale: 2, opacity: 0 }, // 애니메이션 시작 시 상태
-    //         {
-    //             scale: 1,
-    //             opacity: 1,
-    //             duration: 1, // 애니메이션 지속 시간
-    //             ease: 'power2.out', // 애니메이션의 easing 함수
-    //             scrollTrigger: {
-    //                 trigger: h2Ref.current, // 애니메이션 트리거가 되는 요소
-    //                 start: 'top center', // 애니메이션 시작 시점 (h2가 화면 중앙에 도달했을 때)
-    //                 end: 'top top', // 애니메이션 종료 시점 (h2가 화면 상단을 지나면)
-    //                 markers: true,
-    //                 scrub: true, // 스크롤에 따라 애니메이션을 동기화
-    //                 onEnter: () => {
-    //                     gsap.to(ulRef.current, { autoAlpha: 1, duration: 0.5, opacity: 0 });
-    //                 },
-    //             },
-    //         }
-    //     );
+    const handleMouseOver = (index) => {
+        setHoverIndex(index);
+    };
 
-    //     gsap.set(ulRef.current, { autoAlpha: 0 });
-    // }, []);
+    const handleMouseOut = () => {
+        setHoverIndex(null);
+    };
+    const boxes = [
+        {
+            id: 1,
+            title: '나와 닮은 짱구 등장인물 찾기',
+            subtitle: 'mbti 심리테스트',
+            imgSrc: '/project_mbti.png',
+            className: 'box1',
+            url: 'https://mbti-test-orpin.vercel.app/',
+            githubUrl: 'https://github.com/UKYUNGAH/MBTI-TEST',
+        },
+        {
+            id: 2,
+            title: '개인 포트폴리오',
+            subtitle: 'Portfolio of Kyungah',
+            imgSrc: '/project_port.png',
+            className: 'box2',
+            url: 'https://kyungah-portfolio.vercel.app/',
+            githubUrl: 'https://github.com/UKYUNGAH/kyungah-portfolio',
+        },
+        {
+            id: 3,
+            title: 'our life',
+            subtitle: '익명의 자유게시판 커뮤니티',
+            imgSrc: '/project_our.gif',
+            className: 'box3',
+            url: 'https://our-life-nu.vercel.app/',
+            githubUrl: 'https://github.com/UKYUNGAH/Our-Life',
+        },
+        {
+            id: 4,
+            title: "[팀프로젝트] It's Yours",
+            subtitle: '대학 전공서적 중고거래 플랫폼',
+            imgSrc: '/project_itsyours.gif',
+            className: 'box4',
+            url: '',
+            githubUrl: 'https://github.com/UKYUNGAH/ItsYours',
+        },
+        {
+            id: 5,
+            title: '[팀프로젝트] Flora & Dér',
+            subtitle: '꽃을 중심으로 다양한 아이템과 서비스를 제공하는 쇼핑몰',
+            imgSrc: '/project_flo.png',
+            className: 'box5',
+            url: '',
+            githubUrl: 'https://github.com/UKYUNGAH/floraAndDer-Frontend',
+        },
+        {
+            id: 6,
+            title: 'KAKAO BOOK',
+            subtitle: '카카오 API를 활용한 간단한 도서검색 사이트',
+            imgSrc: '/project_kakao.png',
+            className: 'box6',
+            url: 'https://ukyungah.github.io/kakaoBook/',
+            githubUrl: 'https://github.com/UKYUNGAH/kakaoBook',
+        },
+        {
+            id: 7,
+            title: 'NOTES App',
+            subtitle: '로컬스토리지를 활용한 노트 앱',
+            imgSrc: '/project_memoapp.png',
+            className: 'box7',
+            url: 'https://ukyungah.github.io/notesApp/',
+            githubUrl: 'https://github.com/UKYUNGAH/notesApp',
+        },
+        {
+            id: 8,
+            title: '루이스폴센',
+            subtitle: '루이스폴센 리디자인 웹사이트',
+            imgSrc: '/project_lou.gif',
+            className: 'box8',
+            url: 'https://ukyungah.github.io/louispoulsen/',
+            githubUrl: 'https://github.com/UKYUNGAH/louispoulsen',
+        },
+        {
+            id: 9,
+            title: '템버린즈',
+            subtitle: '템버린즈 리디자인 웹사이트',
+            imgSrc: '/project_tam.gif',
+            className: 'box9',
+            url: 'https://ukyungah.github.io/tamburins/',
+            githubUrl: 'https://github.com/UKYUNGAH/tamburins',
+        },
+    ];
 
     return (
         <div className="project">
             <div className="container">
                 <div className="project_wrap">
-                    <h2 ref={h2Ref} className="h2-animation">
-                        Project
-                    </h2>
-
-                    <ul ref={ulRef} className="ul-animation">
-                        <li className="li1">
-                            <div className="box">
-                                <h3>our life</h3>
-                                <h4>익명의 자유게시판</h4>
-                                <h5>view more</h5>
-                            </div>
-                        </li>
-                        <li className="li2">
-                            <div className="box"></div>
-                        </li>
-                        <li className="li1">
-                            <div className="box"></div>
-                        </li>
-                        <li className="li2">
-                            <div className="box"></div>
-                        </li>
-                        <li className="li1">
-                            <div className="box"></div>
-                        </li>
-                        <li className="li2">
-                            <div className="box"></div>
-                        </li>
-                        <li className="li1">
-                            <div className="box"></div>
-                        </li>
-                        <li className="li2">
-                            <div className="box"></div>
-                        </li>
+                    <h2>Project</h2>
+                    <ul>
+                        {boxes.map((box, index) => (
+                            <li key={box.id} className={index % 2 === 0 ? 'li1' : 'li2'}>
+                                <div
+                                    className={`box ${box.className} ${hoverIndex === index ? 'on' : ''}`}
+                                    onMouseOver={() => handleMouseOver(index)}
+                                    onMouseOut={handleMouseOut}
+                                >
+                                    <div className="box_inner">
+                                        <div className="contents">
+                                            <h3>{box.title}</h3>
+                                            <h4>{box.subtitle}</h4>
+                                            <h5>&#123; view more &#125;</h5>
+                                            <img src={box.imgSrc} alt="" />
+                                        </div>
+                                        <div className="overlay">
+                                            <div className="inner">
+                                                <p>{box.title}</p>
+                                                {box.url && <Link to={box.url}>배포사이트 바로가기</Link>}
+                                                <Link to="/">Github 바로가기</Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
