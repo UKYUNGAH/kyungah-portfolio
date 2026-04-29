7fea48dae13b74964e37bc823a4ad33a7e3881a2
경아 <ukyunga0.1@gmail.com>
2026-04-29 23:46:45 +0900

    feat: Google Analytics 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,6 +58,17 @@ export default function RootLayout({ children }) {
                         }),
                     }}
                 />
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-EW79J2N3TG" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EW79J2N3TG');
+            `,
+                    }}
+                />
             </head>
             <body>{children}</body>
         </html>
